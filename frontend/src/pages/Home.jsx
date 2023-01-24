95473ebe08658e6f7596e0b8d1077f180b904f41
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2023-01-24 18:48:14 +0100

    small changes for deployment

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -22,18 +22,21 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+        const response = await fetch(
+          "https://dalle-e-ftwr.onrender.com/api/v1/post",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
         if (response.ok) {
           const result = await response.json();
           setAllPosts(result.data.reverse());
         }
       } catch (error) {
-        allert(error);
+        alert(error);
       } finally {
         setLoading(false);
       }
